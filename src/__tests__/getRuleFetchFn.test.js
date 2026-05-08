83b712703c832d7edf605f80f9136014625a33be
Serban Stancu <stancu@adobe.com>
2026-05-08 10:44:37 -0600

    Log fetch errors correctly.

--- src/__tests__/getRuleFetchFn.test.js
+++ src/__tests__/getRuleFetchFn.test.js
@@ -171,6 +171,64 @@ describe('getRuleFetchFn', () => {
     });
   });
 
+  test('returns a function that logs a 4xx response as an error', () => {
+    const logger = createNewLogger({ ruleId: 1 });
+    const ruleFetchFn = getRuleFetchFn(createFakeFetch(404), [], {}, logger);
+
+    return ruleFetchFn('http://www.google.com').then((r) => {
+      expect(r.status).toBe(404);
+      expect(logger.getJsonLogs()).toStrictEqual([
+        {
+          attributes: { logLevel: 'error' },
+          context: { ruleId: 1 },
+          messages: [
+            '🚀',
+            'FETCH',
+            'Resource',
+            'http://www.google.com',
+            'Options',
+            '{"headers":{}}',
+            'Response Status',
+            '404',
+            'Response Body',
+            'http://www.google.com:arrayBuffer'
+          ],
+          name: 'evaluatingRule',
+          timestampMs: expect.any(Number)
+        }
+      ]);
+    });
+  });
+
+  test('returns a function that logs a 5xx response as an error', () => {
+    const logger = createNewLogger({ ruleId: 1 });
+    const ruleFetchFn = getRuleFetchFn(createFakeFetch(500), [], {}, logger);
+
+    return ruleFetchFn('http://www.google.com').then((r) => {
+      expect(r.status).toBe(500);
+      expect(logger.getJsonLogs()).toStrictEqual([
+        {
+          attributes: { logLevel: 'error' },
+          context: { ruleId: 1 },
+          messages: [
+            '🚀',
+            'FETCH',
+            'Resource',
+            'http://www.google.com',
+            'Options',
+            '{"headers":{}}',
+            'Response Status',
+            '500',
+            'Response Body',
+            'http://www.google.com:arrayBuffer'
+          ],
+          name: 'evaluatingRule',
+          timestampMs: expect.any(Number)
+        }
+      ]);
+    });
+  });
+
   test('returns a function that logs a fetch error', () => {
     const fetchWithError = () => Promise.reject(new Error('some error'));
 
